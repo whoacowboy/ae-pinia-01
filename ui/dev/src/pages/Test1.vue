a077d5bf2c412fc5ad6b747cf6e00bcb15dd0388
James Butler <james@whoacowboy.com>
2024-12-13 08:50:10 -0500

    Adding initial checkin

--- ui/dev/src/pages/Test1.vue
+++ ui/dev/src/pages/Test1.vue
@@ -1,12 +1,16 @@
 <template>
   <q-page padding>
-    
-    <PiniaInDev />
-    
-    
+    <PiniaInDev /><br />
+    {{ devCounter }}
+    <q-btn @click="devCounterStore.increment()" label="Increase Dev" />
   </q-page>
 </template>
-
+<script setup>
+import { storeToRefs } from "pinia";
+import { useDevCounterStore } from "../stores/dev-counter";
+const devCounterStore = useDevCounterStore();
+const { counter: devCounter } = storeToRefs(devCounterStore);
+</script>
 <style lang="sass" scoped>
 .directive-target
   width: 50px
---
+++ ui/src/components/Component.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="component">
+    {{ componentCounter }}
+    <q-btn
+      @click="componentCounterStore.increment()"
+      label="Increase Component"
+    />
+  </div>
+</template>
+<script setup>
+import { useComponentCounterStore } from '../stores/component-counter'
+const componentCounterStore = useComponentCounterStore()
+const { counter: componentCounter } = storeToRefs(componentCounterStore)
+</script>
+<style lang="scss">
+.component {
+}
+</style>
